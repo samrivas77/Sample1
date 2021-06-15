2a1fa89ba1a7d0dd8227dd2841599083ddaf3fb9
Samuel Rivas <rivasalazar77@gmail.com>
2021-06-15 10:32:02 -0600

    Intercambio inicial de proyecto

---
+++ Sample.java
@@ -0,0 +1,32 @@
+/**
+ * Write a description of class Sample here.
+ * 
+ * @author Sam 
+ * @version (a version number or a date)
+ */
+public class Sample
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Sample
+     */
+    public Sample()
+    {
+        // initialise instance variables
+        x = 5;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
